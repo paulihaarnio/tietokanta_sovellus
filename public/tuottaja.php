ae9f907282f977529343f30058ede98a512b50a8
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-02 12:27:11 +0300

    tuottajan lisäys

--- public/tuottaja.php
+++ public/tuottaja.php
@@ -2,7 +2,7 @@
 include('../src/templates/head.php');
 echo "<h1>Tuottajan lisäys</h1>";
     echo'
-    <form action="http://localhost" method="post">
+    <form action="add_tuottaja.php" method="post">
     <label>Tuottajan nimi</label>
     <input type="text" name="nimi"> <br>
     <label>Tuottajan syntymävuosi</label>
