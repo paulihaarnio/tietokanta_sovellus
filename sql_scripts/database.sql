24ac881386fac8998448edb2ce4d2530632b9021
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-02 10:54:33 +0300

    korjaukset

--- sql_scripts/database.sql
+++ sql_scripts/database.sql
@@ -2,11 +2,10 @@ drop database if EXISTS tietokantasovellus;
 CREATE database tietokantasovellus;
 use tietokantasovellus;
 
-drop if EXISTS artisti(
-)
+drop TABLE if EXISTS artisti;
 CREATE TABLE artisti(
     id int not null PRIMARY KEY auto_increment,
     nimi varchar(150),
     svuosi int(4),
     genre varchar(150)
-)
+);
