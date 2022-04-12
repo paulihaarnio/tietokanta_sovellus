ee1ec0a9e681fcb239357d175720ccbef3900c6c
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-12 13:14:41 +0300

    lisätty funktio

--- src/modules/db.php
+++ src/modules/db.php
@@ -1,13 +1,16 @@
 <?php
-$ini = parse_ini_file($_SERVER['DOCUMENT_ROOT']."/tietokanta_sovellus/myconf.ini", true);
-$host = $ini['host'];
-$database = $ini['database'];
-$user = $ini['username'];
-$password = $ini['password'];
-$dsn = "mysql:host=$host;dbname=$database;charset=utf8";
-try {
-	$pdo = new PDO($dsn, $user, $password);
-    //echo "Connected!";
-} catch (PDOException $e) {
-	echo $e->getMessage();
+function getPdoConnection() {
+	$ini = parse_ini_file($_SERVER['DOCUMENT_ROOT']."/tietokanta_sovellus/myconf.ini", true);
+	$host = $ini['host'];
+	$database = $ini['database'];
+	$user = $ini['username'];
+	$password = $ini['password'];
+	$dsn = "mysql:host=$host;dbname=$database;charset=utf8";
+	try {
+		$pdo = new PDO($dsn, $user, $password);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	return $pdo;
 }
