65ba72e6a918d276bb621501e5dd2a5783eeffee
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-13 12:43:31 +0300

    genre dropdown

--- src/templates/dropdown.php
+++ src/templates/dropdown.php
@@ -1,7 +1,6 @@
 <?php
 
 //Dropdown johon listaantuu kaikkien lisättyjen artistien nimet
-
 function createArtistDropdown($selectedId = -1) {
     include MODULES_DIR.'add_artisti.php';
 
@@ -15,6 +14,25 @@ function createArtistDropdown($selectedId = -1) {
         . $a["artistiID"] . '"'
         .($a["artistiID"] == $selectedId ? ' selected' : ''). '>'
         . $a["artistiNimi"]
+        .'</option>';
+    }
+    echo "</select><br/>";
+}
+
+//Dropdown johon listaantuu kaikki lisätyt genret
+function createGenreDropdown($selectedId = -1) {
+    include MODULES_DIR.'add_genre.php';
+
+    $genres = getGenres();
+
+    echo '<select name="genre" id="genre">';
+
+    //loopataan artistit läpi
+    foreach($genres as $g) {
+        echo '<option value="'
+        . $g["genreID"] . '"'
+        .($g["genreID"] == $selectedId ? ' selected' : ''). '>'
+        . $g["genreNimi"]
         .'</option>';
     }
     echo "</select><br/>";
