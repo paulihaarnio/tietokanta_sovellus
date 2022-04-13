66369c265161fbe0d361f1551279cb98b1bad654
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-13 12:53:29 +0300

    albumin lisäys korjattu toimivaksi

--- public/albumi.php
+++ public/albumi.php
@@ -1,19 +1,33 @@
-<?php
-include('../src/templates/head.php');
-echo "<h1>Albumin lisäys</h1>";
-    echo'
-    <form action="../src/modules/add_albumi.php" method="post">
+<?php 
+include TEMPLATES_DIR.'head.php';
+include TEMPLATES_DIR.'dropdown.php';
+include MODULES_DIR.'add_albumi.php'; 
+
+    $artistID = filter_input(INPUT_POST, "artisti", FILTER_SANITIZE_NUMBER_INT);
+    $genreID = filter_input(INPUT_POST, "genre", FILTER_SANITIZE_NUMBER_INT);
+    $albumName = filter_input(INPUT_POST, "albumiNimi");
+    $year = filter_input(INPUT_POST, "vuosi");
+    $producer = filter_input(INPUT_POST, "tuottaja");
+
+    if(isset($artistID) || isset($genreID)) {
+        AddAlbum($artistID, $genreID, $albumName, $year, $producer);
+    }
+
+    $selectedID = isset($artistID) ? $artistID : 0;
+    $selectedGenreID = isset($genreID) ? $genreID : 0;
+?>
+
+    <h1>Albumin lisäys</h1>
+    <form action="albumi.php" method="post">
+    <label>Artisti <?php createArtistDropdown($selectedID); ?></label> <br>
+    <label>Artisti <?php createGenreDropdown($selectedGenreID); ?></label> <br>
     <label>Albumin nimi</label>
     <input type="text" name="albumiNimi"> <br>
     <label>Albumin tekovuosi</label>
-    <input type="text" name="vuosi"><br>
-    <label>Albumin genre</label>
-    <input type="text" name="genre">  <br>
-    <label>Albumin artisti</label>
-    <input type="text" name="artisti">  <br>  
+    <input type="text" name="vuosi"><br> 
     <label>Albumin tuottaja</label>
     <input type="text" name="tuottaja">  <br>
     <input type="submit" value="Lisää albumi">
-    </form>';
+    </form>
 
-include('../src/templates/foot.php');
+<?php include('../src/templates/foot.php'); ?>
--- src/modules/add_albumi.php
+++ src/modules/add_albumi.php
@@ -1,39 +1,36 @@
 <?php
-require 'db.php'; // DB connection
 
-//Filtteroidaan POST-inputit (ei käytetä string-filtteriä, koska deprekoitunut)
-//Jos parametria ei löydy, funktio palauttaa null
-$name = filter_input(INPUT_POST, "albumiNimi");
-$year = filter_input(INPUT_POST, "vuosi");
-$artist = filter_input(INPUT_POST, "artisti");
-$producer = filter_input(INPUT_POST, "tuottaja");
+function AddAlbum($artistID, $genreID, $albumName, $year, $producer) {
+    require_once MODULES_DIR.'db.php'; // DB connection
 
-//Tarkistetaan onko muttujia asetettu
-if( !isset($name) || !isset($year) || !isset($artist) || !isset($producer) ){
-    echo "Parametreja puuttui!! Ei voida lisätä albumia!";
-    exit;
-}
+    //Tarkistetaan onko muuttujia asetettu
+    if( !isset($albumName) || !isset($year) || !isset($artistID) || !isset($genreID) || !isset($producer) ){
+        echo "Parametreja puuttui!! Ei voida lisätä albumia!";
+        exit;
+    }
 
-//Tarkistetaan, ettei tyhjiä arvoja muuttujissa
-if( empty($name) || empty($year) || empty($artist) || empty($producer)){
-    echo "Et voi asettaa tyhjiä arvoja!!";
-    exit;
-}
+    //Tarkistetaan, ettei tyhjiä arvoja muuttujissa
+    if( empty($albumName) || empty($year) || empty($producer)){
+        echo "Et voi asettaa tyhjiä arvoja!!";
+        exit;
+    }
 
-try{
-    $pdo = getPdoConnection();
-    //Suoritetaan parametrien lisääminen tietokantaan.
-    $sql = "INSERT INTO albumi (albumiNimi, tekovuosi, artistiNimi, tuottaja) VALUES (?, ?, ?, ?)";
-    $statement = $pdo->prepare($sql);
-    $statement->bindParam(1, $name);
-    $statement->bindParam(2, $year);
-    $statement->bindParam(3, $artist);
-    $statement->bindParam(4, $producer);
-    
-    $statement->execute();
+    try{
+        $pdo = getPdoConnection();
+        //Suoritetaan parametrien lisääminen tietokantaan.
+        $sql = "INSERT INTO albumi (albumiNimi, tekovuosi, artistiID, genreID, tuottaja) VALUES (?, ?, ?, ?, ?)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(1, $albumName);
+        $statement->bindParam(2, $year);
+        $statement->bindParam(3, $artistID);
+        $statement->bindParam(4, $genreID);
+        $statement->bindParam(5, $producer);
+        
+        $statement->execute();
 
-    echo "Albumi ".$name." on lisätty tietokantaan"; 
-}catch(PDOException $e){
-    echo "Albumia ei voitu lisätä<br>";
-    echo $e->getMessage();
+        echo "Albumi ".$albumName." on lisätty tietokantaan"; 
+    }catch(PDOException $e){
+        echo "Albumia ei voitu lisätä<br>";
+        echo $e->getMessage();
+    }
 }
