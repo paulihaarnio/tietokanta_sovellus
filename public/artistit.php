70536446b1ba676f3c16a65c8a3907b7c6a8d0f3
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-15 14:17:24 +0300

    peruttu muutokset

--- public/artistit.php
+++ public/artistit.php
@@ -19,7 +19,7 @@
 
             <?php
                 foreach($artists as $artist) {
-                    echo "<tr><td><a onClick=\"getSongs($artist[artistiID])\">".$artist["artistiNimi"]."</a></td><td>" . $artist["svuosi"]."</td><td>" . $artist["maa"]."</td></tr>";
+                    echo "<tr><td>".$artist["artistiNimi"]."</td><td>" . $artist["svuosi"]."</td><td>" . $artist["maa"]."</td></tr>";
                 }
 
             ?>
