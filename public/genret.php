ba8d0c1e2eae9ef0595f14267a82c3c4a394756b
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-15 11:25:19 +0300

    genre  listaus

---
+++ public/genret.php
@@ -0,0 +1,26 @@
+<?php 
+    include TEMPLATES_DIR.'head.php';
+    include MODULES_DIR.'add_genre.php';
+
+    //hae kaikki albumit tietokannasta
+    $genres = getGenres();
+
+?>
+
+    <h2>Albumit</h2>
+    <table class="table table-striped">
+        <tr>
+            <th>Genre</th>
+        </tr>
+    
+
+        <?php
+            foreach($genres as $g) {
+                echo "<tr><td>".$g["genreNimi"]. "</td></tr>";
+            }
+
+        ?>
+    </table>
+
+
+<?php include TEMPLATES_DIR.'foot.php';?>
