7219d1ecfe517e4ecfa88275465a8a6f6b3faa1b
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-15 11:14:16 +0300

    albumien listaus

---
+++ public/albumit.php
@@ -0,0 +1,29 @@
+<?php 
+    include TEMPLATES_DIR.'head.php';
+    include MODULES_DIR.'add_albumi.php';
+
+    //hae kaikki albumit tietokannasta
+    $albums = getAlbums();
+
+?>
+
+    <h2>Albumit</h2>
+    <table class="table table-striped">
+        <tr>
+            <th>Nimi</th>
+            <th>Tekovuosi</th>
+            <th>Artisti</th>
+            <th>Tuottaja</th>
+        </tr>
+    
+
+        <?php
+            foreach($albums as $a) {
+                echo "<tr><td>".$a["albumiNimi"]."</td><td>" . $a["tekovuosi"]."</td><td>" . $a["artistiID"]."</td><td>" . $a["tuottaja"]. "</td></tr>";
+            }
+
+        ?>
+    </table>
+
+
+<?php include TEMPLATES_DIR.'foot.php';?>
