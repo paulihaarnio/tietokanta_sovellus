46b94dcc90f2beaa8781a6780f1fdf3b17c263f0
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-04 12:58:51 +0300

    poistettu console log funktio

--- public/kappaleet.php
+++ public/kappaleet.php
@@ -8,15 +8,6 @@
     $inPlaylist = songsInPlaylist($userID);
 
     $id = filter_input(INPUT_GET, "id");
-    function console_log($output, $with_script_tags = true) {
-        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-    ');';
-        if ($with_script_tags) {
-            $js_code = '<script>' . $js_code . '</script>';
-        }
-        echo $js_code;
-    }
-    console_log($id);
 
     if(isset($id)) {
         try{
