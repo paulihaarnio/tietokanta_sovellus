a70d3b835b41a255d7f4682b68da9273431e4bdd
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-07 13:11:58 +0300

    lisätty kappaleen muokkaus

--- public/kappale.php
+++ public/kappale.php
@@ -6,30 +6,61 @@ include MODULES_DIR.'add_kappale.php';
     $artistID = filter_input(INPUT_POST, "artisti", FILTER_SANITIZE_NUMBER_INT);
     $songName = filter_input(INPUT_POST, "kappaleNimi");
     $time = filter_input(INPUT_POST, "kesto");
+    $songIDFromUrl = filter_input(INPUT_GET, "id");
+    $selectedID = isset($artistID) ? $artistID : 0;
+    $songNameUrl = filter_input(INPUT_GET, "name");
+    $timeFromUrl = filter_input(INPUT_GET, "time");
 
-    if(isset($artistID)) {
+    if(isset($songIDFromUrl)) {
         try {
-            addSong($artistID, $songName, $time);
-            echo '<div class="alert alert-success" role="alert">Kappale '.$songName .' lisätty!</div>';
+            updateSong($songIDFromUrl, $songName, $artistID, $time);
+            //echo '<div class="alert alert-success" role="alert">Kappaleen '.$songName .' tiedot päivitetty!</div>';
         }catch(Exception $e) {
             echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
         }
-        
+
+        ?>
+
+        <div class="main-container">
+            <h1>Kappaleen muokkaus</h1>
+            <form action="muokkaa.php?id=<?php echo $songIDFromUrl; ?>" method="post">
+                <label class="dropdown">Artisti <?php createArtistDropdown($selectedID); ?></label>
+                <label>Kappaleen nimi</label>
+                <input type="text" name="kappaleNimi" placeholder="<?php echo $songNameUrl; ?>">
+                <label>Kappaleen kesto</label>
+                <input type="text" name="kesto" placeholder="<?php echo $timeFromUrl; ?>">
+                <input type="submit" value="Tallenna tiedot">
+            </form>
+        </div>
+
+        <?php
+    
+    }  
+    else {
+
+        if(isset($artistID)) {
+            try {
+                addSong($artistID, $songName, $time);
+                echo '<div class="alert alert-success" role="alert">Kappale '.$songName .' lisätty!</div>';
+            }catch(Exception $e) {
+                echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+            }
+            
+        }
+
+    ?>
+        <div class="main-container">
+            <h1>Kappaleen lisäys</h1>
+            <form action="kappale.php" method="post">
+                <label class="dropdown">Artisti <?php createArtistDropdown($selectedID); ?></label>
+                <label>Kappaleen nimi</label>
+                <input type="text" name="kappaleNimi">
+                <label>Kappaleen kesto</label>
+                <input type="text" name="kesto">
+                <input type="submit" value="Lisää kappale">
+            </form>
+        </div>
+
+<?php 
     }
-
-    $selectedID = isset($artistID) ? $artistID : 0;
-
-?>
-    <div class="main-container">
-        <h1>Kappaleen lisäys</h1>
-        <form action="kappale.php" method="post">
-            <label class="dropdown">Artisti <?php createArtistDropdown($selectedID); ?></label>
-            <label>Kappaleen nimi</label>
-            <input type="text" name="kappaleNimi">
-            <label>Kappaleen kesto</label>
-            <input type="text" name="kesto">
-            <input type="submit" value="Lisää kappale">
-        </form>
-    </div>
-
-<?php include TEMPLATES_DIR.'foot.php'; ?>
+include TEMPLATES_DIR.'foot.php'; ?>
