797559215308602bd47658b4ea3ad05ffeaf590d
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-08 12:52:25 +0300

    artistin muokkaus

--- public/artisti.php
+++ public/artisti.php
@@ -8,6 +8,39 @@ include MODULES_DIR.'add_artisti.php';
     $year = filter_input(INPUT_POST, "svuosi");
     $country = filter_input(INPUT_POST, "maa");
 
+    $artistIDFromUrl = filter_input(INPUT_GET, "id");
+    $nameUrl = filter_input(INPUT_GET, "name");
+    $yearUrl = filter_input(INPUT_GET, "year");
+    $countryUrl = filter_input(INPUT_GET, "country");
+
+    if(isset($artistIDFromUrl)) {
+        try {
+            updateArtist($artistIDFromUrl, $name, $year, $country);
+        }catch(Exception $e) {
+            echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+        }
+
+        ?>
+
+        <div class="main-container">
+            <h1>Artistin muokkaus</h1>
+            <form action="artisti.php?id=<?php echo $artistIDFromUrl; ?>" method="post">
+                <label>Artistin nimi</label>
+                <input type="text" name="nimi" placeholder="<?php echo $nameUrl; ?>">
+                <label>Artistin syntymävuosi</label>
+                <input type="text" name="svuosi" placeholder="<?php echo $yearUrl; ?>">
+                <label>Syntymämaa</label>
+                <input type="text" name="maa" placeholder="<?php echo $countryUrl; ?>"> 
+                <input type="submit" value="Tallenna tiedot">
+            </form>
+        </div>
+
+        <?php
+    
+    }  
+
+    else {
+
     if(isset($name)) {
         try{
             addArtist($name, $year, $country);
@@ -30,4 +63,6 @@ include MODULES_DIR.'add_artisti.php';
         </form>
     </div>
 
-<?php include TEMPLATES_DIR.'foot.php'; ?>
+<?php 
+    }
+include TEMPLATES_DIR.'foot.php'; ?>
